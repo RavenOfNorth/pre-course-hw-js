6bc79544cf225b3ba4516361467c5d6bd6a4a68c
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-05-21 20:13:22 +0300

    added check of a random number for evenness or oddness with 1 console.log

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -7,8 +7,7 @@ let number1 = Math.floor(Math.random() * 100)
 
 let number1 = Math.floor(Math.random() * 100);
 
-if (number1 % 2 === 0) {
-    console.log(number1 + " " + "четное число")
-} else {
-    console.log(number1 + " " + "нечетное число")
-}
+let remainder = number1 % 2;
+let oddOrPary = (remainder === 0) ? number1 + " :Это четное число": number1 + " :Это нечетное число";
+
+console.log(oddOrPary);
