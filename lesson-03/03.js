4e5e6e05e5f48ec361e813b50cfcfb025b9cc44a
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-06-18 19:35:54 +0300

    ex.3
    lesson 3

---
+++ lesson-03/03.js
@@ -0,0 +1,8 @@
+const fruits = ["яблоко", "банан", "апельсин", "манго", "киви", "ананас", "виноград", "груша", "слива", "персик"];
+
+function fruitNinja() {
+    fruitsArray = fruits.slice(1, fruits.length - 2);
+    console.log (fruitsArray);
+}
+
+fruitNinja();
