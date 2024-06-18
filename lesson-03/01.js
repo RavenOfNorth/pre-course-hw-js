be6aabcece8460f9f98cbec87672a0fde79674dd
RavenOfNorth <dhaurylenka.job@yandex.by>
2024-06-18 18:54:34 +0300

    Update 01.js
    lesson

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,6 +1,6 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
-function createColorString() {
+function createColorString(color) {
      return colors.
      filter(colors => color === "черный" || color === "красный" || color === "желтый").
      join("-");
